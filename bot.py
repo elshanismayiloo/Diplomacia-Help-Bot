3558c9d70fadc0ad0db0a270053c3f9f95a7876b
Elshan <78827424+elshanismayiloo@users.noreply.github.com>
2026-07-13 01:57:17 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -252,7 +252,8 @@ async def collect_production(update: Update, context: ContextTypes.DEFAULT_TYPE)
     context.user_data["price_step"] = 0
     await update.message.reply_text(
         "İndiki bazar qiyməti neçədir?\n"
-        lines.append("(bu resurs üçün gəlir hesablamaq istəmirsənsə, aşağıdakı düyməni basa bilərsən)"),
+        lines.append("")
+        f"(bu resurs üçün gəlir hesablamaq istəmirsənsə, aşağıdakı düyməni basa bilərsən)",
         reply_markup=SKIP_PRICE_KB,
     )
     return COLLECT_PRICE
